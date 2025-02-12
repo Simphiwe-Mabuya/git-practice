8e94d5fc12ca739cdd2653b02d30a68169d44822
Simpra11 <mrmabuya11@gmail.com>
2025-02-12 12:28:19 +0200

    Moved to Project folder

--- test.js
+++ test.js
@@ -1,25 +1,7 @@
-let quote = document.querySelector("#quote");
-let author = document.querySelector("#author");
-let btn = document.querySelector("#new-quote");
+let monsters = {canada: 'Sasquatch', nepal: 'Yeti', scotland: 'Loch Ness'};
+let pets = ['bob', 'rex', 'spot']
 
-function pickFromArray(choices) {
-
-    return choices[Math.floor(Math.random() * choices.length)];
+for (prop in monsters){
+    console.log(`In ${prop} we have the ${monsters[prop]}`)
+    
 }
-
-btn.addEventListener("click", () => {
-    let randomQuote = pickFromArray(quotes);
-
-    let text = randomQuote.quote;
-    let person = randomQuote.author;
-
-    quote.textContent = text;
-    author.textContent = person;
-
-});
-
-window.onload = () => {
-    let firstQuote = quotes[2]; // first quote just after the page loads
-    quote.textContent = firstQuote.quote;
-    author.textContent = firstQuote.author;
-};
